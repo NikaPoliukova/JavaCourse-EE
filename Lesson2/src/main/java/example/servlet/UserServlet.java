379bf7cf1e3153db1883416f9d45a46a127477af
NikaPoliukova <nikanarchuk@gmail.com>
2022-10-14 16:47:34 +0400

    add UserServlet

---
+++ Lesson2/src/main/java/example/servlet/UserServlet.java
@@ -0,0 +1,35 @@
+package example.servlet;
+
+import example.model.User;
+import example.service.UserService;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+
+    private UserService userService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        userService = (UserService) config.getServletContext().getAttribute("userService");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        final List<User> users = userService.findUsers();
+        req.setAttribute("users", users);
+        getServletContext().getRequestDispatcher("/users.jsp").forward(req, resp);
+    }
+}
