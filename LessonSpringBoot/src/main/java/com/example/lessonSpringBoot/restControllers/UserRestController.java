2a1f1803ba731e67615fb7fa8e2869321f531db4
NikaPoliukova <nikanarchuk@gmail.com>
2022-12-13 15:24:52 +0400

    add rest controller and postman collection

--- LessonSpringBoot/src/main/java/com/example/lessonSpringBoot/restControllers/UserRestController.java
+++ LessonSpringBoot/src/main/java/com/example/lessonSpringBoot/restControllers/UserRestController.java
@@ -8,11 +8,7 @@ import com.example.lessonSpringBoot.service.FriendsServiceImpl;
 import com.example.lessonSpringBoot.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,19 +26,13 @@ public class UserRestController {
   private final UserConverter userConverter;
 
 
-  @GetMapping("/{pageNumber}")
-  public List<UserDtoRest> getUsers(@RequestParam(required = false) String searchValue,
-                                    @PathVariable Integer pageNumber) {
-    int pageSize = 5;
+  @GetMapping("/users")
+  public List<UserDtoRest> getUsers(@RequestParam(name = "searchValue", required = false) String searchValue,
+                                    @RequestParam(value = "pageNumber", required = false, defaultValue = "1") Integer pageNumber,
+                                    @RequestParam(value = "pageSize", required = false, defaultValue = "2") Integer pageSize) {
+
     Page<User> page = userService.getFilteredUsers(searchValue, pageNumber - 1, pageSize);
     List<User> users = page.getContent();
     return userConverter.toDto(users);
-  }
-
-  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-  public UserDtoRest createFriendRequest(@RequestBody long friendUserId) {
-     friendsService.createFriendRequest(authContext.getUserId(), friendUserId);
-
-    return userConverter.toDto(userService.findUserByUserName(authContext.getUserName()));
   }
 }
