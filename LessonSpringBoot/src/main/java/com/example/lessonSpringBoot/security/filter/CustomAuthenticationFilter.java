233c06e53202b05bdd9ff5bfd3169e100dc562c6
NikaPoliukova <nikanarchuk@gmail.com>
2022-12-27 13:49:00 +0400

    correct filter

--- LessonSpringBoot/src/main/java/com/example/lessonSpringBoot/security/filter/CustomAuthenticationFilter.java
+++ LessonSpringBoot/src/main/java/com/example/lessonSpringBoot/security/filter/CustomAuthenticationFilter.java
@@ -7,14 +7,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static com.example.lessonSpringBoot.security.jwt.JwtUtils.generateAccessToken;
@@ -22,7 +21,7 @@ import static com.example.lessonSpringBoot.security.jwt.JwtUtils.generateRefresh
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Log4j2
-public class CustomAuthenticationFilter extends GenericFilterBean {
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
   private final AuthenticationManager authenticationManager;
   private final String secretKey;
@@ -31,7 +30,7 @@ public class CustomAuthenticationFilter extends GenericFilterBean {
     this.authenticationManager = authenticationManager;
     this.secretKey = secretKey;
   }
-/*
+
   @Override
   public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
     String username = request.getParameter("userName");
@@ -55,29 +54,4 @@ public class CustomAuthenticationFilter extends GenericFilterBean {
     response.setContentType(APPLICATION_JSON_VALUE);
      new ObjectMapper().writeValue(response.getOutputStream(), dto);
   }
-
- */
-
-  @Override
-  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-    HttpServletRequest request = (HttpServletRequest) servletRequest;
-    String username = request.getParameter("userName");
-    String password = request.getParameter("password");
-    log.info("username is {}", username);
-    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-    Authentication authentication =  authenticationManager.authenticate(authenticationToken);
-    User user = (User) authentication.getPrincipal();
-    Algorithm algorithm = Algorithm.HMAC256(secretKey.getBytes());
-    String requestUrl = request.getRequestURL().toString();
-
-    String accessToken = generateAccessToken(user, requestUrl, algorithm);
-    String refreshToken = generateRefreshToken(user, requestUrl, algorithm);
-    TokensDto dto = new TokensDto();
-    dto.setAccess_token(accessToken);
-    dto.setRefreshToken(refreshToken);
-    servletResponse.setContentType(APPLICATION_JSON_VALUE);
-    new ObjectMapper().writeValue(servletResponse.getOutputStream(), dto);
-  }
-
-
 }
